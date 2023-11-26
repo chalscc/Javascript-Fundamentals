defecdc0a144528fb22fd4ca285519ef6aae9ee2
chalscc <carlescatalancalabuig@gmail.com>
2023-11-26 13:38:00 +0100

    template string

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-const nombre = 'Carles';
-const apellido = 'Calabuig';
-
-// Template string
-// const nombreCompleto = nombre + ' ' + apellido;
-const nombreCompleto = `${nombre} ${apellido}`;
-
-console.log(nombreCompleto);
