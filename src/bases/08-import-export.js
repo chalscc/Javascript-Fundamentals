4dbdfa95353d6aa26314e6276a496b9b0d54ef58
chalscc <carlescatalancalabuig@gmail.com>
2023-11-26 15:49:24 +0100

    Importaciones/Exportaciones

---
+++ src/bases/08-import-export.js
@@ -0,0 +1,16 @@
+import heroes, { owners } from '../data/heroes';
+
+console.log(heroes)
+
+const getHeroeById = (id) => {
+    return heroes.find((heroe) =>  heroe.id === id);
+}
+
+console.log(getHeroeById(3));
+
+const getHeroesByOwner = (owner) => {
+    return heroes.filter((heroe) =>  heroe.owner === owner);
+}
+
+console.log(getHeroesByOwner('DC'));
+console.log(owners);
